fed4d326d5507a719dc235f31073010c21aaf0b5
Abenitov <adrianbenitovalverde@gmail.com>
2017-06-13 23:06:59 +0200

    second commit

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../node_modules/@types/body-parser/index.d.ts" />
+/// <reference path="../node_modules/@types/express/index.d.ts" />
+/// <reference path="../node_modules/@types/mime/index.d.ts" />
+/// <reference path="../node_modules/@types/serve-static/index.d.ts" />
+/// <reference path="../node_modules/@types/errorhandler/index.d.ts" />
